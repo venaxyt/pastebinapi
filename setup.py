4355848d8a49854dd11e5087eb5c654abdc8d40a
Venax <81310818+venaxyt@users.noreply.github.com>
2021-07-03 00:15:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,21 @@ classifiers = [
   "Intended Audience :: Education",
   "Operating System :: Microsoft :: Windows :: Windows 10",
   "License :: OSI Approved :: MIT License",
-  "Programming Language :: Python :: 3.9.6"
+  "Programming Language :: Python :: 3.9"
 ]
  
 setup(
   name="pastebinapi",
-  version="0.0.1",
+  version="0.0.4",
   description="This module allows to use Pastebin API very easily.",
   long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
   url="https://github.com/venaxyt/pastebinapi",  
   author="Venax",
   author_email="venaxumofficial@gmail.com",
   license="MIT", 
-  classifiers=classifiers,
   keywords="pastebin", 
-  install_requires=["requests"] 
+  classifiers=classifiers,
+  install_requires=["requests"],
+  packages=["pastebinapi"]
 )
