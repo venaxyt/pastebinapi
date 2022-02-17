fe7c86f87c820b1e3b43ab0a301dfe7f4989eca7
ks <92867218+ksIsCute@users.noreply.github.com>
2022-02-17 11:12:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
  
 setup(
   name="pastebinapi",
-  version="0.0.4",
+  version="0.0.5",
   description="This module allows to use Pastebin API very easily.",
   long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
